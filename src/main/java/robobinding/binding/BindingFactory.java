ae95e2ee9be2ef51a69ce940ef0bff842d6267bb
Robert Taylor <robert.taylor426@gmail.com>
2011-09-24 19:58:51 +0100

    Added BindingFactory and BindingMap classes

---
+++ src/main/java/robobinding/binding/BindingFactory.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2011 Cheng Wei, Robert Taylor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package robobinding.binding;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.LayoutInflater.Factory;
+import android.view.View;
+
+/**
+ * @since 1.0
+ * @version $Revision: 1.0 $
+ * @author Robert Taylor
+ *
+ */
+public class BindingFactory implements Factory
+{
+	private final LayoutInflater layoutInflater;
+	private HashMap<View, BindingMap> viewBindingsMap = new HashMap<View, BindingMap>();
+
+	public BindingFactory(LayoutInflater layoutInflater)
+	{
+		this.layoutInflater = layoutInflater;
+	}
+
+	public View onCreateView(String name, Context context, AttributeSet attrs)
+	{
+		try
+		{
+			View view = layoutInflater.createView(name, null, attrs);
+			viewBindingsMap.put(view, BindingMap.createFrom(attrs));
+			return view;
+		} 
+		catch (InflateException e)
+		{
+			throw new RuntimeException(e);
+		} 
+		catch (ClassNotFoundException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	public Map<View, BindingMap> getViewBindingsMap()
+	{
+		return viewBindingsMap;
+	}
+}
---
+++ src/test/java/robobinding/binding/BindingMapTest.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2011 Cheng Wei, Robert Taylor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package robobinding.binding;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import android.util.AttributeSet;
+
+/**
+ * @since 1.0
+ * @version $Revision: 1.0 $
+ * @author Robert Taylor
+ *
+ */
+public class BindingMapTest
+{
+	@Test
+	public void givenAnAttributeSetWithNoBindingAttributes_WhenCreatingBindingMap_ThenReturnEmptyMap()
+	{
+		BindingMap bindingMap = BindingMap.createFrom(MockAttributeSet.withNoBindingAttributes());
+		assertTrue(bindingMap.isEmpty());
+	}
+	
+	@Test
+	public void givenAnAttributeSetWithXBindingAttributes_WhenCreatingBindingMap_ThenReturnMapWithXEntries()
+	{
+		int numberOfBindingAttributes = anyNumber();
+		int numberOfNonBindingAttributes = anyNumber();
+		BindingMap bindingMap = BindingMap.createFrom(MockAttributeSet.withAttributes(numberOfBindingAttributes, numberOfNonBindingAttributes));
+		assertThat(bindingMap.size(), equalTo(numberOfBindingAttributes));
+	}
+	
+	@Test
+	public void givenAnAttributeSetWithBindingAttributes_WhenCreatingBindingMap_ThenBindingMapKeysShouldMapToCorrectValues()
+	{
+		int numberOfBindingAttributes = anyNumber();
+		int numberOfNonBindingAttributes = anyNumber();
+		AttributeSet attributeSetWithAttributes = MockAttributeSet.withAttributes(numberOfBindingAttributes, numberOfNonBindingAttributes);
+		BindingMap bindingMap = BindingMap.createFrom(attributeSetWithAttributes);
+		
+		for (String attribute : bindingMap.keySet())
+			assertThat(bindingMap.get(attribute), equalTo(attributeSetWithAttributes.getAttributeValue(BindingMap.ROBOBINDING_NAMESPACE, attribute)));
+	}
+	
+	private int anyNumber()
+	{
+		return new Random().nextInt(100);
+	}
+}
