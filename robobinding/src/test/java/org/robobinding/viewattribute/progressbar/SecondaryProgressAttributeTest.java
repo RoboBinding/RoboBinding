f5542f6700b4560a83f0eb4adf383ae295adb7c6
Robert Taylor <robert.taylor426@gmail.com>
2012-01-04 13:17:37 +0000

    Added ignore to test failing because of Robolectric

--- robobinding/src/test/java/org/robobinding/viewattribute/progressbar/SecondaryProgressAttributeTest.java
+++ robobinding/src/test/java/org/robobinding/viewattribute/progressbar/SecondaryProgressAttributeTest.java
@@ -18,6 +18,7 @@ package org.robobinding.viewattribute.progressbar;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.robobinding.viewattribute.AbstractPropertyViewAttributeTest;
 import org.robobinding.viewattribute.RandomValues;
@@ -30,6 +31,7 @@ import android.widget.ProgressBar;
  * @version $Revision: 1.0 $
  * @author Robert Taylor
  */
+@Ignore //Waiting for Robolectric (https://github.com/pivotal/robolectric/pull/167)
 public class SecondaryProgressAttributeTest extends AbstractPropertyViewAttributeTest<ProgressBar, SecondaryProgressAttribute>
 {
 	@Test
