5d0e3535f9d91dcb25a29d7b80f765c0e6b79cd0
weicheng113 <weicheng112@gmail.com>
2014-04-26 16:07:43 +0800

    #113 Added supported binding attribute doc in the format of javadoc.

---
+++ robobinding/src/main/java/org/robobinding/viewattribute/absspinner/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Binding implementations and Supported Binding Attributes for
+ * <a href="http://developer.android.com/reference/android/widget/AbsSpinner.html">android.widget.AbsSpinner</a>.
+ *
+ * @prop dropdownLayout; Integer, Static resource; no
+ *
+ */
+package org.robobinding.viewattribute.absspinner;
---
+++ robobinding/src/main/java/org/robobinding/viewattribute/edittext/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Binding implementations and Supported Binding Attributes for
+ * <a href="http://developer.android.com/reference/android/widget/EditText.html">android.widget.EditText</a>.
+ *
+ * @prop text; CharSequence, String; yes
+ *
+ * @event onTextChanged; {@link org.robobinding.viewattribute.edittext.TextChangedEvent}
+ *
+ */
+package org.robobinding.viewattribute.edittext;
