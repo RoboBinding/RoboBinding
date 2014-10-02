d22da088089d396265c26026de5ee3289c8f4856
weicheng113 <weicheng112@gmail.com>
2014-10-02 22:19:30 +0800

    #48 Initailize Code Generation for PresentationModel and ItemPresentationModel.

---
+++ framework/src/main/java/org/robobinding/property/PropertyDescriptor.java
@@ -0,0 +1,77 @@
+package org.robobinding.property;
+
+import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.Set;
+
+import com.google.common.base.Joiner;
+
+
+/**
+ * @since 1.0
+ * @author Cheng Wei
+ *
+ */
+public class PropertyDescriptor {
+	private Class<?> beanType;
+	private Class<?> propertyType;
+	private String name;
+	private boolean readable;
+	private boolean writable;
+	
+	public PropertyDescriptor(Class<?> beanType, Class<?> propertyType, String name, boolean readable, boolean writable) {
+		this.beanType = beanType;
+		this.propertyType = propertyType;
+		this.name = name;
+		this.readable = readable;
+		this.writable = writable;
+	}
+
+	public Class<?> getPropertyType() {
+		return propertyType;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void checkReadWriteProperty(boolean isReadWriteProperty) {
+		checkReadable();
+		if (isReadWriteProperty) {
+			checkWritable();
+		}
+	}
+	
+	private void checkReadable() {
+		if (!readable) {
+			throw new RuntimeException("The property " + getShortDescription() + " is not readable");
+		}
+	}
+
+	private void checkWritable() {
+		if (!writable) {
+			throw new RuntimeException("The property " + getShortDescription() + " is not writable");
+		}
+	}
+	
+	public String getShortDescription() {
+		return PropertyUtils.shortDescription(beanType, name);
+	}
+
+	public String getDescription() {
+		return decriptionWithDependencies(Collections.<String>emptySet());
+	}
+
+	public String decriptionWithDependencies(Set<String> dependentProperties) {
+		String description = MessageFormat.format(
+				"property(name:{0}, propertyType:{1}, isReadable:{2}, isWritable:{3}, beanType:{4}", 
+				name, propertyType.getName(), readable, writable, beanType.getName());
+
+		if (!dependentProperties.isEmpty()) {
+			description += MessageFormat.format(", dependencies:{0}", Joiner.on(",").join(dependentProperties));
+		}
+		description += ")";
+		return description;
+	}
+
+}
---
+++ framework/src/main/java/org/robobinding/property/PropertyUtils.java
@@ -0,0 +1,48 @@
+package org.robobinding.property;
+
+import java.text.MessageFormat;
+import java.util.Set;
+
+import org.robobinding.internal.java_beans.BeanInfo;
+import org.robobinding.internal.java_beans.IntrospectionException;
+import org.robobinding.internal.java_beans.Introspector;
+import org.robobinding.internal.java_beans.PropertyDescriptor;
+
+import com.google.common.collect.Sets;
+
+/**
+ * 
+ * @since 1.0
+ * @version $Revision: 1.0 $
+ * @author Cheng Wei
+ */
+public class PropertyUtils {
+	private static final Set<String> EXCLUDED_PROPERTY_NAMES = Sets.newHashSet("class");
+
+	public static Set<String> getPropertyNames(Class<?> beanClass) {
+		try {
+			BeanInfo info = Introspector.getBeanInfo(beanClass);
+			PropertyDescriptor[] propertyDescriptorArray = info.getPropertyDescriptors();
+
+			Set<String> propertyNames = Sets.newHashSet();
+			for (PropertyDescriptor propertyDescriptor : propertyDescriptorArray) {
+				if (EXCLUDED_PROPERTY_NAMES.contains(propertyDescriptor.getName())) {
+					continue;
+				}
+				
+				propertyNames.add(propertyDescriptor.getName());
+			}
+			return propertyNames;
+		} catch (IntrospectionException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static String shortDescription(Class<?> beanClass, String proeprtyName) {
+		return MessageFormat.format("{0}.{1}", beanClass.getName(), proeprtyName);
+
+	}
+
+	private PropertyUtils() {
+	}
+}
