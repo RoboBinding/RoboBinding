13422229ad5a49aaef5bd0225f3bf582ae0f4113
weicheng113 <weicheng112@gmail.com>
2015-09-08 21:25:23 +1000

    Fixed issue #233. org.robobinding.annotation.@ViewBinding should allow empty simpleOneWayProperties.

--- framework/src/main/java/org/robobinding/annotation/ViewBinding.java
+++ framework/src/main/java/org/robobinding/annotation/ViewBinding.java
@@ -14,6 +14,6 @@ import java.lang.annotation.Target;
 @Target({ ElementType.TYPE })
 public @interface ViewBinding {
 
-	String[] simpleOneWayProperties();
+	String[] simpleOneWayProperties() default {};
 
 }
